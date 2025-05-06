7b446889f6954b73f58938c42573720b595e94b3
mabagupv <mabaguena.upv@gmail.com>
2025-05-06 11:50:12 +0200

    cambio desde ordenador

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+//cambiosdfsd dkj kjas kjhas dfkjhfasfdja dlaijdf lk
+
 public class Spawn : MonoBehaviour
 {
     GameObject spawn;
