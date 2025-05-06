206f31906936aac91c86dc01b36e798eaf8bb148
mabag <129035046+mabagupv@users.noreply.github.com>
2025-05-06 11:49:50 +0200

    Update Spawn.cs
    otra persoa

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//esto tmambioen es un cambio
+
+
+
 public class Spawn : MonoBehaviour
 {
     GameObject spawn;
